ada7a5b6b4786d0a0ff16d320f4c74fbf02fadcb
goldey17 <goldey17@up.edu>
2015-09-18 10:00:50 -0700

    Second commit

--- app/src/main/java/com/example/administrator/fantasysoccerapp/second_activity.java
+++ app/src/main/java/com/example/administrator/fantasysoccerapp/second_activity.java
@@ -6,18 +6,18 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class team_screen extends ActionBarActivity {
+public class second_activity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_team_screen);
+        setContentView(R.layout.activity_second_activity);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_team_screen, menu);
+        getMenuInflater().inflate(R.menu.menu_second_activity, menu);
         return true;
     }
 
