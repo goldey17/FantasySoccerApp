d212c38401869bf8b70c67463b23ddcc35ef9701
goldey17 <goldey17@up.edu>
2015-09-22 20:39:04 -0700

    Finished all layouts except for the third one

--- app/src/main/java/com/example/administrator/fantasysoccerapp/first_activity.java
+++ app/src/main/java/com/example/administrator/fantasysoccerapp/first_activity.java
@@ -12,27 +12,6 @@ public class first_activity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_activity);
-    }
+    }//onCreate
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_first_activity, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-}
+}//first_activity
